7cadcf8dd25ec7b621593619e22b420fcd67cc3f
seregakras <seregakras@yandex.ru>
2023-08-14 20:36:04 +0300

    PP_2_1_4_spring-types-of-wiring

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,14 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Egg6 {
+
+    @Autowired
+    private Deth8 deth;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + deth.toString();
     }
 }
